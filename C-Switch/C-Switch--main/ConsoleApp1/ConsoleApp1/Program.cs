94a4ead7974a7f7efca6f03b4f7f6338a97d4242
Nika <122999874+Phutko13@users.noreply.github.com>
2023-05-15 20:41:31 +0400

    Add files via upload

---
+++ C-Switch/C-Switch--main/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Airchiet romelime varianti : \n 1.Luw/kentoba 2.kalkulatori");
+            string starting = Console.ReadLine();
+            switch(starting){
+                case "1":{
+                        Console.WriteLine("Sheiyvanet ricxvi : ");
+                        double number = Convert.ToInt32(Console.ReadLine());
+                        if (number % 2 == 0) Console.WriteLine("Luwia");
+                        else Console.WriteLine("Kentia");
+                        break;
+                    }  
+                case "2":{
+                    Console.WriteLine("Sheiyvanet pirveli ricxvi : ");
+                    double number1 = Convert.ToInt32(Console.ReadLine());
+                    Console.WriteLine("Sheiyvanet meore ricxvi : ");
+                    double number2 = Convert.ToInt32(Console.ReadLine());
+                    Console.WriteLine("Sheiyvanet operacia \n 1.+ 2.- 3.* 4./ ");
+                    string ending = Console.ReadLine();
+                    double number3 = number1 - number2;
+                    switch(ending){
+                        case "1":{
+                            Console.WriteLine(number1 + " + " + number2 + " = " + number1+number2);
+                            break;
+                        }
+                        case "2":{
+                            Console.WriteLine(number1 + " - " + number2 + " = " + number3);
+                            break;
+                        }
+                        case "3":{
+                            Console.WriteLine(number1 + " / " + number2 + " = " + number1/number2);
+                            break;
+                        }
+                        case "4":{
+                            Console.WriteLine(number1 + " * " + number2 + " = " + number1*number2);
+                            break;
+                        }
+                        default:{
+                            Console.WriteLine("Araswori sheyvana");
+                            break;
+                        }
+                    }
+                        
+                }
+                default:{
+                    Console.WriteLine("Araswori sheyvana");
+                    break;
+                }
+            }
+        }
+    }
+}
